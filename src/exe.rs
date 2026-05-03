a43ea05d00a4331c17127fc05f04a13d18d031c2
Ryuga <vaishnav2353@gmail.com>
2026-05-03 11:44:40 +0530

    Execution with isolate boxes

--- src/exe.rs
+++ src/exe.rs
@@ -3,8 +3,10 @@ use crate::languages::get_handler;
 use crate::loader::get_lang_config;
 use crate::models::{Req, Resp};
 use crate::runner::safe_execute;
+use crate::workers::IsolateBox;
 
-pub fn execute_code(req: Req, passed_token: Option<String>) -> Result<Resp, String>{
+
+pub fn execute_code(isolate_box: &IsolateBox, req: Req, passed_token: Option<String>) -> Result<Resp, String>{
     let lang_config = get_lang_config(req.language.as_str());
 
     if lang_config.authenticate {
@@ -27,9 +29,9 @@ pub fn execute_code(req: Req, passed_token: Option<String>) -> Result<Resp, Stri
 
     let handler = get_handler(req.language.as_str(), lang_config.clone());
 
-    let work_dir = tempdir().map_err(|e| e.to_string())?;
+    let work_dir = &isolate_box.path;
 
-    let program = match handler.prepare(work_dir.path(), &req.code) {
+    let program = match handler.prepare(work_dir, &req.code) {
         Ok(p) => p,
         Err(e) => {
             return Ok(Resp{
@@ -42,12 +44,18 @@ pub fn execute_code(req: Req, passed_token: Option<String>) -> Result<Resp, Stri
     };
 
     let exe_cmd = if lang_config.compile {
-        format!("{} && {}", handler.compile_cmd(&program).join(" "), handler.run_cmd(&program).join(" "))
+        format!(
+            "{} && {}",
+            handler.compile_cmd(&program).join(" "),
+            handler.run_cmd(&program).join(" ")
+        )
     }
     else {
         handler.run_cmd(&program).join(" ")
     };
 
-    let (output, std_log, code, time_ms) = safe_execute(work_dir.path(), lang_config.clone(), exe_cmd)?;
+    let (output, std_log, code, time_ms) =
+        safe_execute(&isolate_box, lang_config.clone(), exe_cmd)?;
+
     Ok(Resp{output, std_log, code, time_ms})
 }
